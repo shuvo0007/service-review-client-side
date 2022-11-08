30978b441327e30ac6876bf3d466e9374221df35
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-11-08 10:45:00 +0600

    see all button

--- src/Components/Layouts/Outlets/Main/Main.js
+++ src/Components/Layouts/Outlets/Main/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { SlArrowRight } from "react-icons/sl";
 import { useLoaderData } from "react-router-dom";
 import CategoryCards from "./CategoryCards";
 
 const Main = () => {
   const allCategories = useLoaderData();
   return (
-    <div>
+    <div className="">
       <div className="md:grid md:grid-cols-3 md:gap-4 md:pl-72 md:pr-52 md:pt-16 flex flex-col justify-center items-center pt-10">
         {allCategories
           .slice(-3)
@@ -17,9 +18,11 @@ const Main = () => {
             ></CategoryCards>
           ))}
       </div>
-      <button className="text-white text-xl border-2 border-red-600 p-5 rounded-xl ">
-        See All
-      </button>
+      <div className="flex justify-center">
+        <button className="text-white text-xl border-2 border-blue-600 p-5 rounded-xl flex ">
+          See All <SlArrowRight size={25} />
+        </button>
+      </div>
     </div>
   );
 };
