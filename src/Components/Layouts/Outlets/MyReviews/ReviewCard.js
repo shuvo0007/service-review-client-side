0b116b376d0cdd6bc1f9b7b6f017357a86d8fec5
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-11-10 13:17:54 +0600

    delete review done

--- src/Components/Layouts/Outlets/MyReviews/ReviewCard.js
+++ src/Components/Layouts/Outlets/MyReviews/ReviewCard.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdDeleteForever } from "react-icons/md";
 import { TbEdit } from "react-icons/tb";
 import { Link } from "react-router-dom";
 
 const ReviewCard = ({ user, reviews }) => {
+  const [reviewDisplay, setReviewDisplay] = useState(reviews);
+
+  const handleDelete = (review) => {
+    const agree = window.confirm(
+      `Are you sure you want to delete: ${reviews.review}`
+    );
+    console.log(agree);
+    if (agree) {
+      console.log("Deleting review with id:", reviews._id);
+      fetch(`http://localhost:5000/reviews/${reviews._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("Review deleted Successfully");
+            const remainingUsers = reviewDisplay.filter(
+              (usr) => usr._id !== reviews._id
+            );
+            console.log("ami asi", remainingUsers);
+            setReviewDisplay(remainingUsers);
+          }
+        });
+    }
+  };
+
   return (
     <div>
       {user.uid == reviews.userId ? (
@@ -17,12 +44,11 @@ const ReviewCard = ({ user, reviews }) => {
           </h2>
           <div className="float-right text-white flex">
             <Link to={`/private/update/${reviews._id}`}>
-                <TbEdit size={40} />
-
+              <TbEdit size={40} />
             </Link>
-            <button>
+            <Link onClick={() => handleDelete(reviews.review)}>
               <MdDeleteForever size={40} />
-            </button>
+            </Link>
           </div>
           <p>
             Review: <span className="text-2xl">{reviews.review}</span>
