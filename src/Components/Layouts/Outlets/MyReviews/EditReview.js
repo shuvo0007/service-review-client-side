486970e23f67a47018013ceda95a36c862e5b4c1
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-11-27 17:09:51 +0600

    mod

--- src/Components/Layouts/Outlets/MyReviews/EditReview.js
+++ src/Components/Layouts/Outlets/MyReviews/EditReview.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 import useTitle from "../../../../Hooks/useTitle";
 
--- src/Components/Layouts/Outlets/MyReviews/ReviewCard.js
+++ src/Components/Layouts/Outlets/MyReviews/ReviewCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { MdDeleteForever } from "react-icons/md";
 import { TbEdit } from "react-icons/tb";
 import { Link } from "react-router-dom";
